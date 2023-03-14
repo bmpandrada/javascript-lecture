ce5030a67b8323acd49e05b095f8e171f0b40042
bmpandrada <bmpandrada@gmail.com>
2023-03-14 22:27:46 +0800

    adding lecture

--- script.js
+++ script.js
@@ -27,17 +27,17 @@
 //     mike
 // );
 
-const ageCal = (x) => {
-    return 2023 - x;
-}
+// const ageCal = (x) => {
+//     return 2023 - x;
+// }
 
-const yearss = [1990, 1992, 2000];
+// const yearss = [1990, 1992, 2000];
 
-const answ1 =  ageCal(yearss[0])
-const answ2 =  ageCal(yearss[1])
-const answ3 =  ageCal(yearss[yearss.length - 1])
-const answer  = [answ1, answ2, answ3]
-console.log(answer);
+// const answ1 =  ageCal(yearss[0])
+// const answ2 =  ageCal(yearss[1])
+// const answ3 =  ageCal(yearss[yearss.length - 1])
+// const answer  = [answ1, answ2, answ3]
+// console.log(answer);
 
 // const person = {
 //     fname:'Milo',
@@ -87,9 +87,59 @@ console.log(answer);
 
 
 //inverted loop
-const person = [
-    'bruce', 'web developer', 'coffee lover','banana', 22 ['Bea', 'Phil', 'Kath', 'Saki', 'Vits', 'Chy']
-];
-for(let i = person.length - 1; i >= 0; i--){
-    console.log(i, person[i])
+// const person = [
+//     'bruce', 'web developer', 'coffee lover','banana', 22 ['Bea', 'Phil', 'Kath', 'Saki', 'Vits', 'Chy']
+// ];
+// for(let i = person.length - 1; i >= 0; i--){
+//     console.log(i, person[i])
+// }
+
+// console.log('===============');
+
+// const exercise = ['shoulder', 'bicep','tricep']
+// const prog = ['2sets, reps 12', '3sets, reps 8','4sets, reps 6']
+
+// for(let i = 0; i < exercise.length; i++){
+//     console.log(i, exercise[i]);
+//     for(let i = 0; i < prog.length; i++){
+//         console.log(prog[i]);
+//     }
+// }
+
+// let exercise = ['shoulder', 'bicep','tricep','back']
+// while(exercise.length){
+//     console.log(exercise); 
+//     exercise++
+// }
+
+// let dice = Math.trunc(Math.random() * 6) + 1;
+// while(dice !== 6){
+//     console.log(`dice --> ${dice}`);
+//     dice = Math.trunc(Math.random() * 6) + 1;
+//     if(dice === 6) console.log('end of loop');
+// }
+
+
+const calcTip = (bills) => {
+    return bills >= 50 && bills <= 300 ? bills * 0.15 : bills * 0.2;
 }
+
+let bills = [22, 295, 176, 440, 37, 105, 10,1100,86,52]
+const tips = []
+let totals = []
+for(let i = 0; i < bills.length; i++){
+    const tip = calcTip(bills[i]);
+    tips.push(tip);
+    totals.push(tip + bills[i]);
+}
+
+console.log(tips, totals);
+const calcAverage = (arr) => {
+    let sum = 0;
+    for(let i = 0; i < arr.length; i++){
+        sum += arr[i]
+    }
+    console.log(sum);
+}
+
+calcAverage([5,5])
