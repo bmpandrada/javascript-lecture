8dd38bc7db16a61e408eed518d8218851eba1636
Bruce Andrada <88586952+bmpandrada@users.noreply.github.com>
2023-03-17 03:22:10 +0000

    update apps js

--- app.js
+++ app.js
@@ -2,43 +2,56 @@
 
 const number = document.querySelector('.number');
 const scored = document.querySelector('.score');
-let highscore = document.querySelector('.highscore');
 const btnCheck = document.querySelector('.check');
 const btnAgain = document.querySelector('.again');
 
-
-let score = 20;
+let highscore = document.querySelector('.highscore');
 let message = document.querySelector('.message');
-let displayMessage = function(x){ message.textContent=x}
+let score = 20; //create newState for score
+let displayMessage = function(x){ message.textContent=x}//newState for message
 let secretNumber = (Math.trunc(Math.random()*20)+1)
-console.log(secretNumber);
 
-btnCheck.addEventListener('click',function(){
-  
+let randomNumber = function(){
+    let random = (Math.trunc(Math.random()*20)+1)
+    secretNumber = random;
+    console.log(secretNumber);
+    return secretNumber
+}
+
+
+btnCheck.addEventListener('click',function(){  
     let guess = Number(document.querySelector('.guess').value);
     console.log(guess, typeof guess);
     if(!guess){
-        displayMessage('not a number')
-        
+        displayMessage('not a number')       
     }else if(guess === secretNumber){
         displayMessage('you guess')
-      
         highscore.textContent=score;
         number.style.width ='30rem'
         number.textContent=secretNumber;
         document.body.style.background='#60b347';
-       
     }else{
         if(score > 1){
-            displayMessage(guess > secretNumber ? 'to high' : 'to low')
-            score--;
+            displayMessage(guess > secretNumber ? 'to high' : 'to low')//itenary string
+            score--; //decrease setState
             scored.textContent=score
         }else{
             displayMessage('you lost')
             scored.textContent=0;
             document.body.style.background='red';
-        }
-       
+        }     
     }
 })
 
+//reset
+btnAgain.addEventListener('click', function(){
+    randomNumber()
+    let score = 20;
+    number.textContent='?'
+    highscore.textContent=0;
+    scored.textContent=score;
+    number.style.width =''
+    displayMessage('Start guessing...')
+    Number(document.querySelector('.guess').value='')
+    document.body.style.background='#222';
+})
