992c6f347e84d06cdd3e4b995e57de2177016983
Bruce Andrada <88586952+bmpandrada@users.noreply.github.com>
2023-03-20 03:29:15 +0000

    refactor

--- modal.js
+++ modal.js
@@ -5,14 +5,9 @@ const btnClose = document.querySelector('.btnClose');
 
 const stateModal = () => modalContainer.classList.add('show')
 const setModal = () => modalContainer.classList.remove('show')
-
 for(let i = 0;i < btn.length; i++) btn[i].addEventListener('click',stateModal)
 btnClose.addEventListener('click', setModal)
+const closeKey = (e) => e.key === 'Escape' && modalContainer.classList.contains('show') && setModal()
+document.addEventListener('keydown', closeKey)
 
-document.addEventListener('keydown', function(e){
-    if(e.key === 'Escape' && modalContainer.classList.contains('show')){
-        setModal();
-        console.log(e);
-    }
-})
-
+ 
