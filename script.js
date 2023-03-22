2eb057dcf0035ec5af25638bb486d874eb83ea49
Bruce Andrada <88586952+bmpandrada@users.noreply.github.com>
2023-03-22 02:05:16 +0000

    update object literals

--- script.js
+++ script.js
@@ -423,40 +423,40 @@ btn.addEventListener('click',function(){
 
 'use strict';
 
-// Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+// // Data needed for a later exercise
+// const flights =
+//   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
-// Data needed for first part of the section
-const restaurant = {
-  name: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze, Italy',
-  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+// // Data needed for first part of the section
+// const restaurant = {
+//   name: 'Classico Italiano',
+//   location: 'Via Angelo Tavanti 23, Firenze, Italy',
+//   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+//   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+//   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
-  // order: function(mainA, mainB){
-  //   return [this.starterMenu[mainA], this.mainMenu[mainB]]
-  // },
-  order: (ing1, ing2, ing3) =>{
-    return console.log(`${restaurant.starterMenu[ing1]} nice ${ing2} nice nice ${ing3}`);
-  }
+//   openingHours: {
+//     thu: {
+//       open: 12,
+//       close: 22,
+//     },
+//     fri: {
+//       open: 11,
+//       close: 23,
+//     },
+//     sat: {
+//       open: 0, // Open 24 hours
+//       close: 24,
+//     },
+//   },
+//   // order: function(mainA, mainB){
+//   //   return [this.starterMenu[mainA], this.mainMenu[mainB]]
+//   // },
+//   order: (ing1, ing2, ing3) =>{
+//     return console.log(`${restaurant.starterMenu[ing1]} nice ${ing2} nice nice ${ing3}`);
+//   }
   
-};
+// };
 
 // console.log(restaurant.openingHours);
 
@@ -677,3 +677,32 @@ const restaurant = {
 //   console.log(`${item + 1}: ${product}`);
 // }
 //Enhanced object literals
+
+
+// Data needed for a later exercise
+const openingHours = {
+  thu: {
+    open: 12,
+    close: 22,
+  },
+  fri: {
+    open: 11,
+    close: 23,
+  },
+  sat: {
+    open: 0,
+    close: 24,
+  },
+};
+
+
+const restaurant = {
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  openingHours,
+  order: (ing1, ing2, ing3) =>{
+    return console.log(`${restaurant.starterMenu[ing1]} nice ${restaurant.starterMenu[ing2]} nice nice ${restaurant.starterMenu[ing3]}`);
+  }
+};
+
+console.log(restaurant);
+restaurant.order(1,2,3);
